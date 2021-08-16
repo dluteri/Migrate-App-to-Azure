7c07586ad1ca5e8888c27d9efeac7cacab3dda18
dluteri <donna.luteri@gmail.com>
2021-08-16 18:25:13 -0500

    1st commit

---
+++ Project 3/web/config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="luteripostgresdb.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="udacityadmin@luteripostgresdb" #TODO: Update value
+    POSTGRES_PW="redacted"   #TODO: Update value
+    POSTGRES_DB="luteripostgresdb"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://luteriservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=O1Bzk0xkU4vHrP5uLuNmxrctyvKMgifbS7cCQd+acC8=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
